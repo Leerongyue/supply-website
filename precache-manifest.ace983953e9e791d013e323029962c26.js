fd3f1b9924714d31e18a687ca910fd2e6ac842d3
lirongyue <761780862@qq.com>
2020-09-22 14:18:14 +0800

    deploy

---
+++ precache-manifest.ace983953e9e791d013e323029962c26.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbbc8606360a9aab23f4dd1fdc25080f",
+    "url": "api.config.ts"
+  },
+  {
+    "revision": "d7f549cbc9332b4aa005",
+    "url": "css/app.f2655468.css"
+  },
+  {
+    "revision": "e29d7e6c80458db7c66c",
+    "url": "css/chunk-vendors.db835868.css"
+  },
+  {
+    "revision": "5949c8f29b19894e10996f61b77498ed",
+    "url": "index.html"
+  },
+  {
+    "revision": "d7f549cbc9332b4aa005",
+    "url": "js/app.463678a8.js"
+  },
+  {
+    "revision": "e29d7e6c80458db7c66c",
+    "url": "js/chunk-vendors.41a0b22a.js"
+  },
+  {
+    "revision": "db7a8a414a8d292161201a6b7865b53e",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
